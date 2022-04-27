0eb083c8a8431dc190b8ca0fe87736fb60730276
akim-dev <78294519+akim-dev@users.noreply.github.com>
2022-04-27 10:39:32 +0700

    Revert "Stash 2022 27 04 10 38 37"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import './style.css';
-
-import Navbar from './components/Navbar';
+import React from "react";
+import "./style.css";
 
 export default function App() {
   return (
     <div>
-      <Navbar />
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
     </div>
   );
 }
