11b796fad1456af5aaf64d92279ad48d3e014ae3
akim-dev <musta.akim@gmail.com>
2022-04-27 11:12:44 +0700

    4 files modified, 6 added, 4 deleted

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <img src="../images/comp.png" className="hero--photo" />
+      <h1 className="hero--header">Online Experience</h1>
+      <p className="hero--content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+        facilis doloribus pariatur error sint? Beatae atque provident aperiam
+        perferendis, iusto nihil doloribus odio tempora nobis rerum? Cum eveniet
+        repudiandae nulla.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <img src="../images/airbnb.png" />
+    </nav>
+  );
+}
+
+export default Navbar;
